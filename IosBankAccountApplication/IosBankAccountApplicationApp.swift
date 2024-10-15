021d44e09566e31f68fa957947ac4d88e5ea2251
PARISOT Romain <rp211085@dgkyqj35jwf2.edu.atlas.local>
2024-10-15 15:51:36 +0200

    first commit

---
+++ IosBankAccountApplication/IosBankAccountApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosBankAccountApplicationApp.swift
+//  IosBankAccountApplication
+//
+//  Created by PARISOT Romain on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct IosBankAccountApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IosBankAccountApplication/MainView.swift
@@ -0,0 +1,8 @@
+//
+//  MainView.swift
+//  IosBankAccountApplication
+//
+//  Created by PARISOT Romain on 15/10/2024.
+//
+
+import Foundation
